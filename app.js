65f8ce9180c53f83b17eb194acd4108a881f952f
sbpinilla <sbpinilla@hotmail.com>
2017-07-25 23:26:47 -0500

    Se agrego la interaccion con el usuario, se agrego texto de resultado y boton de reset , se aplicarion cambios diseño

--- app.js
+++ app.js
@@ -1,13 +1,50 @@
 
+//Deinir valores para las selecciones
+var PIEDRA=1;
+var PAPEL=2;
+var TIJERA=3;
+
 var interval ;
 
+//Iniciar el efecto de seleccion de computador
 window.onload = function() {
      
     interval= setInterval(generarFiguraAleatoria, 400);
-
+    document.getElementById("btnReset").classList.add("resultOff");
+    document.getElementById("btnReset").classList.remove("resultOn");
 };
 
+//Eevnto de seleccion del usuario
+function play(choiceUser){
 
+    var choiceComputer;
+    clearInterval(interval);
+
+    var imgComputer = document.getElementById("imgComputer");
+
+    var computerChoice = Math.random();
+
+        if (computerChoice < 0.34) {
+             imgComputer.src="img/piedra.jpg";
+             choiceComputer=1;
+        } else if(computerChoice <= 0.67) {
+             imgComputer.src="img/papel.jpg";
+             choiceComputer=2;
+        } else {
+             imgComputer.src="img/tijera.jpg";
+             choiceComputer=3;
+        } 
+
+var resultado=compare(choiceUser,choiceComputer);
+
+document.getElementById("result").innerHTML=""+resultado;
+
+document.getElementById("btnReset").classList.add("resultOn");
+document.getElementById("btnReset").classList.remove("resultOff");
+
+}
+
+//Generar figura aleatoria
 function generarFiguraAleatoria(){
 
     var imgComputer = document.getElementById("imgComputer");
@@ -21,59 +58,48 @@ function generarFiguraAleatoria(){
         } else {
              imgComputer.src="img/tijera.jpg";
         } 
-           
+          
 }
-
-
-//Pedir eleccion del usuario
-var userChoice = prompt("Do you choose rock, paper or scissors?");
-
-//Calcular eleccion del computador
-var computerChoice = Math.random();
-if (computerChoice < 0.34) {
-	computerChoice = "rock";
-} else if(computerChoice <= 0.67) {
-	computerChoice = "paper";
-} else {
-	computerChoice = "scissors";
-} 
-console.log("Computer: " + computerChoice);
-
-
-//obtener el resultado del juego
-var resultado=compare(userChoice,computerChoice);
-
-alert("El ganador es "+resultado);
 
 //Funcion que valida la eleccion del usuario y el computador 
 function compare(choice1,choice2){
     
     //Comparar que los resultado no sean iguales 
     if(choice1===choice2){
-        return "The result is a tie!";   
-    }else if (choice1=="rock"){ //Validaciones en caso de que el usuario seleccione roca
+        return "Empate !!";   
+    }else if (choice1==PIEDRA){ //Validaciones en caso de que el usuario seleccione roca
         
-        if(choice2==="scissors"){
-            return "rock wins";   
+        if(choice2===TIJERA){
+            return "Ganaste !";   
         }else{
-            return "paper wins";   
+            return "Perdiste !";   
         }
             
-    }else if (choice1=="paper"){//Validaciones en caso de que el usuario seleccione papel
+    }else if (choice1==PAPEL){//Validaciones en caso de que el usuario seleccione papel
         
-        if(choice2==="rock"){
-            return "paper wins";   
+        if(choice2===PIEDRA){
+            return "Ganaste !!";   
         }else{
-            return "scissors wins";   
+            return "Perdiste";   
         }
    
-    }else if(hoice1=="scissors"){//Validaciones en caso de que el usuario seleccione tijeras
+    }else if(choice1==TIJERA){//Validaciones en caso de que el usuario seleccione tijeras
         
-        if(choice2==="rock"){
-            return "rock wins";   
+        if(choice2===PIEDRA){
+            return "Perdiste !";   
         }else{
-            return "scissors wins";   
+            return "Ganaste";   
         }
         
     }
+}
+
+//Funcion para jugar de nuevo
+function reset(){
+    interval= setInterval(generarFiguraAleatoria, 400);
+    document.getElementById("result").innerHTML="";
+    
+    document.getElementById("btnReset").classList.add("resultOff");
+    document.getElementById("btnReset").classList.remove("resultOn");
+
 }
